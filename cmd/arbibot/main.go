1d86b932ec6bc0641a41c360d8bbbee8a96204a0
notlelouch <notlelouchvb@gmail.com>
2024-12-28 19:58:47 +0530

    refactored main.go

--- cmd/arbibot/main.go
+++ cmd/arbibot/main.go
@@ -4,7 +4,6 @@ import (
 	// "notlelouch/ArbiBot/internal/arbitrage"
 	"context"
 	"log"
-	"notlelouch/ArbiBot/internal/exchange"
 	"notlelouch/ArbiBot/internal/exchange/hyperliquid"
 	"notlelouch/ArbiBot/internal/exchange/kucoin"
 	// Import other exchanges
@@ -21,36 +20,15 @@ func main() {
 
 	// Initialize exchanges
 	hyperliquidClient := hyperliquid.NewHyperliquidWS(true)
-	// Initialize other exchanges
-
-	// Initialize KuCoin WebSocket client
 	kucoinClient := kucoin.NewKuCoinWS(tokenResp)
 
-	exchanges := []exchange.Exchange{hyperliquidClient /*, other exchanges */}
+	// Connect to exchanges via websockets
+	hyperliquidClient.Connect(ctx)
+	kucoinClient.Connect(ctx)
 
-	// Connect to exchanges
-	for _, ex := range exchanges {
-		if err := ex.Connect(ctx); err != nil {
-			log.Fatalf("Failed to connect to exchange: %v", err)
-		}
-	}
-
-	// Subscribe to order books
-	coin := "SOL"
-	for _, ex := range exchanges {
-		if err := ex.SubscribeToOrderBook(coin); err != nil {
-			log.Fatalf("Failed to subscribe to order book: %v", err)
-		}
-	}
-
-	// Connect to WebSocket
-	if err := kucoinClient.Connect(ctx); err != nil {
-		log.Fatalf("Failed to connect to KuCoin WebSocket: %v", err)
-	}
-	// Subscribe to public order book
-	if err := kucoinClient.Subscribe("/market/level2:BTC-USDT", false); err != nil {
-		log.Fatalf("Failed to subscribe to KuCoin order book: %v", err)
-	}
+	// Subscribe to order book
+	hyperliquidClient.SubscribeToOrderBook("SOL")
+	kucoinClient.Subscribe("/market/level2:SOL-USDT", false)
 
 	// Run arbitrage logic
 	// arbitrage.FindArbitrageOpportunities(exchanges, coin)
